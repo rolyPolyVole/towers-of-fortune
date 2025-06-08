72acdd6e066c1a665e5a30e80cc95515de2e5707
rolyPolyVole <yezhanting@gmail.com>
2025-06-08 13:38:58 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "towers-of-fortune"
---
+++ src/main/kotlin/dev/rolyPolyVole/towersoffortune/TowersOfFortune.kt
@@ -0,0 +1,14 @@
+package dev.rolyPolyVole.towersoffortune
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TowersOfFortune : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
